30cebbf61c8eafdbe322ade54ecb748f333e51ba
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-11-01 15:39:35 -0400

    added the navbar info from the database, and refactored the function into the function page and included it and stuff

---
+++ includes/functions.php
@@ -0,0 +1,16 @@
+<?php include 'db.php';?>
+<?php
+
+function navBarCategoriesDisplay(){
+    global $connection;
+    $query = "SELECT * FROM categories";
+    $select_all_categories_query = mysqli_query($connection, $query);
+    while($row = mysqli_fetch_assoc($select_all_categories_query)){
+       $cat_title = $row["cat_title"];
+
+       echo "<li><a href='#'>{$cat_title}</a></li>";
+    }
+
+}
+
+?>
--- includes/nav.php
+++ includes/nav.php
@@ -1,3 +1,7 @@
+
+<?php include 'includes/functions.php' ?>
+
+
 <!-- Navigation -->
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
@@ -14,15 +18,11 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                    <li>
-                        <a href="#">About</a>
-                    </li>
-                    <li>
-                        <a href="#">Services</a>
-                    </li>
-                    <li>
-                        <a href="#">Contact</a>
-                    </li>
+                    <?php
+                    
+                        navBarCategoriesDisplay();
+                    
+                    ?>    
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
