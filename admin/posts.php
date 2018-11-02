f9328f9c3e4d3f1cc5ea2d923ead14529d225d43
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-11-02 17:04:47 -0400

    added the post data into the table

--- admin/posts.php
+++ admin/posts.php
@@ -33,25 +33,45 @@
 
                                 </tr>
                             </thead>
+
+
+
+
                             <tbody>
-                                <tr>
-                                    <td>10</td>
-                                    <td>ME</td>
-                                    <td>Awesome stuff</td>
-                                    <td>Lego</td>
-                                    <td>Approved</td>
-                                    <td>link.png</td>
-                                    <td>cool, lego, stuff</td>
-                                    <td>3</td>
-                                    <td>today</td>
-                                </tr>
+                                <?php                                 
+                                    $query = "SELECT * FROM posts ";
+                                    $posts_query = mysqli_query($connection, $query);                                                            
+                                    while($row = mysqli_fetch_assoc($posts_query)){
+                                        $post_id = $row["post_id"];
+                                        $post_title = $row["post_title"];
+                                        $post_author = $row["post_author"];
+                                        $post_date = $row["post_date"];
+                                        $post_image = $row["post_image"];
+                                        $post_tags = $row["post_tags"];
+                                        $post_comment_count = $row["post_comment_count"];
+                                        $post_status = $row["post_status"];
+                                        $post_category_id = $row["post_category_id"];
+                                        echo"
+                                            <tr>
+                                                <td>$post_id</td>
+                                                <td>$post_author</td>
+                                                <td>$post_title</td>
+                                                <td>$post_category_id</td>
+                                                <td>$post_status</td>
+                                                <td><img src='../images/$post_image' class='img-responsive'></td>
+                                                <td>$post_tags</td>
+                                                <td>$post_comment_count</td>
+                                                <td>$post_date</td>
+                                            </tr>";
+                                    }        
+                                            
+                                ?>
+
+
                             </tbody>
                         </table>
 
 
-                        <div class="col-xs-6">
-                        
-                        </div>
                         
 
 
