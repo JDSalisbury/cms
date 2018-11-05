d03ed73292c898a598d7c01578bd3cd6d3388447
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-11-05 10:09:15 -0500

    added the post info into vars to be put into database.

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -1,3 +1,24 @@
+
+<?php
+if(isset($_POST['create_post'])){
+    $post_title = $_POST['title'];
+    $post_author = $_POST['author'];
+    $post_category_id = $_POST['post_category_id'];
+    $post_status = $_POST['post_status'];
+
+    $post_image = $_FILES['image']['name'];
+    $post_image_temp = $_FILES['image']['tmp_name'];
+
+    $post_tags = $_POST['post_tags'];
+    $post_content = $_POST['post_content'];
+    $post_date = date('d-m-y');
+    $post_comment_count = 4;
+
+}
+
+?>
+
+
 <form action="" method="POST" enctype="multipart/form-data">
 
     <div class="form-group">
@@ -33,6 +54,10 @@
     <div class="form-group">
         <label for="post_content">Post Content</label>
         <textarea class="form-control" name="post_content" cols="30" rows="10"></textarea>
+    </div>
+
+    <div class="form-group">
+        <input class="btn btn-primary" type="submit" name="create_post" value="PUBLISH">
     </div>
 
 </form>
