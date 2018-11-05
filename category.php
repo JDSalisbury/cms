dc81cd419acee49e147fef08f73a05f09eea7eb4
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-11-05 16:02:32 -0500

    added dynamic titles to the link pages

--- category.php
+++ category.php
@@ -6,19 +6,28 @@
     <!-- Page Content -->
     <div class="container">
         <div class="row">
+            <?php
+                if(isset($_GET['c_id'])){
+                    $category_id = $_GET['c_id'];
+                }
 
+                $query = "SELECT * FROM categories WHERE cat_id = $category_id";
+                $select_all_categories_query = mysqli_query($connection, $query);
+                while($row = mysqli_fetch_assoc($select_all_categories_query)){
+                    $cat_title = $row["cat_title"];  
+                    
+                }
+
+            ?>
             <!-- Blog Entries Column -->
             <div class="col-md-8">
                 <h1 class="page-header">
-                    POSTS
+                    <?php echo $cat_title; ?>
                     <small>stuff and things</small>
                 </h1>
 
                 <!-- First Blog Post -->
                 <?php 
-                    if(isset($_GET['c_id'])){
-                        $category_id = $_GET['c_id'];
-                    }
 
                     $query = "SELECT * FROM posts WHERE post_category_id = $category_id";
                     $select_all_posts_query = mysqli_query($connection, $query);
--- post.php
+++ post.php
@@ -5,23 +5,31 @@
     <!-- Page Content -->
     <div class="container">
         <div class="row">
+        <?php
+             if(isset($_GET['p_id'])){
+                $post_to_display_id = $_GET['p_id'];
+            }
+
+            $query = "SELECT * FROM posts WHERE post_id = $post_to_display_id";
+            $select_all_posts_query = mysqli_query($connection, $query);
+            while($row = mysqli_fetch_assoc($select_all_posts_query)){
+                $post_title = $row["post_title"];
+                $post_author = $row["post_author"];
+            }
+        ?>
 
             <!-- Blog Entries Column -->
             <div class="col-md-8">
                 <h1 class="page-header">
-                    POSTS
-                    <small>stuff and things</small>
+                    <?php echo $post_title?>
+                    <small>by <?php echo "<a href='author.php?author=$post_author'>{$post_author}</a>"?> </small>
                 </h1>
 
                 <!-- First Blog Post -->
                 <?php 
 
-                    if(isset($_GET['p_id'])){
-                        $post_to_display_id = $_GET['p_id'];
-                    }
-
-                    $query = "SELECT * FROM posts WHERE post_id = $post_to_display_id";
-                    $select_all_posts_query = mysqli_query($connection, $query);
+                        $query = "SELECT * FROM posts WHERE post_id = $post_to_display_id";
+                        $select_all_posts_query = mysqli_query($connection, $query);
                         while($row = mysqli_fetch_assoc($select_all_posts_query)){
 
                             $post_id = $row['post_id'];
@@ -31,9 +39,7 @@
                             $post_image = $row["post_image"];
                             $post_content = $row["post_content"];
                             
-                            echo "<h2><a href='post.php?p_id=$post_id'>{$post_title}</a></h2>
-
-                                <p class='lead'>by <a href='#'>{$post_author}</a></p>
+                            echo "
                                 <p><span class='glyphicon glyphicon-time'></span>{$post_date}</p>
                                 <hr>
                                 <img class='img-responsive' src='images/{$post_image}' alt=''>
