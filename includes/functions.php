809b4fcaa753ca6946fea49fe546161138bc8284
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-11-05 14:46:53 -0500

    created a page for individual posts, id is the href for the title. click on the title go to the individual post

--- includes/functions.php
+++ includes/functions.php
@@ -58,13 +58,14 @@ function tagSearchBar(){
 
 
 function displaySetupForPost($row){
+    $post_id = $row['post_id'];
     $post_title = $row["post_title"];
     $post_author = $row["post_author"];
     $post_date = $row["post_date"];
     $post_image = $row["post_image"];
     $post_content = $row["post_content"];
     
-    echo "<h2><a href='#'>{$post_title}</a></h2>
+    echo "<h2><a href='post.php?p_id=$post_id'>{$post_title}</a></h2>
 
         <p class='lead'>by <a href='#'>{$post_author}</a></p>
         <p><span class='glyphicon glyphicon-time'></span>{$post_date}</p>
