5ded0a9d4ef18330fd8a6a0fed01f6f16e1073e2
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-11-05 16:33:41 -0500

    grabbing comments from database

--- admin/includes/view_all_comments.php
+++ admin/includes/view_all_comments.php
@@ -3,13 +3,14 @@
                                 <tr>
                                     <th>ID</th>
                                     <th>AUTHOR</th>
-                                    <th>TITLE</th>
-                                    <th>CATEGORY</th>
+                                    <th>COMMENT</th>
+                                    <th>EMAIL</th>
                                     <th>STATUS</th>
-                                    <th>IMAGE</th>
-                                    <th>TAGS</th>
-                                    <th>COMMENTS</th>
+                                    <th>IN RESPONSE TO</th>
                                     <th>DATE</th>
+                                    <th>APPROVE</th>
+                                    <th>UNAPPROVE</th>
+                                    <th>DELETE</th>
                                     <th> </th>
                                 </tr>
                             </thead>
@@ -19,39 +20,38 @@
 
                             <tbody>
                                 <?php                                 
-                                    $query = "SELECT * FROM posts ";
+                                    $query = "SELECT * FROM comments ";
                                     $posts_query = mysqli_query($connection, $query);                                                            
                                     while($row = mysqli_fetch_assoc($posts_query)){
-                                        $post_id = $row["post_id"];
-                                        $post_title = $row["post_title"];
-                                        $post_author = $row["post_author"];
-                                        $post_date = $row["post_date"];
-                                        $post_image = $row["post_image"];
-                                        $post_tags = $row["post_tags"];
-                                        $post_comment_count = $row["post_comment_count"];
-                                        $post_status = $row["post_status"];
-                                        $post_category_id = $row["post_category_id"];
+                                        $comment_id = $row["comment_id"];
+                                        $comment_post_id = $row["comment_post_id"];
+                                        $comment_author = $row["comment_author"];
+                                        $comment_email = $row["comment_email"];
+                                        $comment_content = $row["comment_content"];
+                                        $comment_status = $row["comment_status"];
+                                        $comment_date = $row["comment_date"];
+                                        
 
 
-                                        $query = "SELECT * FROM categories WHERE cat_id = $post_category_id ";
-                                        $cat_query = mysqli_query($connection, $query);
+                                        // $query = "SELECT * FROM categories WHERE cat_id = $post_category_id ";
+                                        // $cat_query = mysqli_query($connection, $query);
                                     
-                                        while($row = mysqli_fetch_assoc($cat_query)){
-                                            $cat_title = $row["cat_title"];
-                                        }
+                                        // while($row = mysqli_fetch_assoc($cat_query)){
+                                        //     $cat_title = $row["cat_title"];
+                                        // }
 
                                         echo"
                                             <tr>
-                                                <td>$post_id</td>
-                                                <td>$post_author</td>
-                                                <td>$post_title</td>
-                                                <td>$cat_title</td>
-                                                <td>$post_status</td>
-                                                <td><img src='../images/$post_image' class='img-responsive'></td>
-                                                <td>$post_tags</td>
-                                                <td>$post_comment_count</td>
-                                                <td>$post_date</td>
-                                                <td><a href='posts.php?delete={$post_id}'><i class='fa fa-trash-o fa-2x' aria-hidden='true'></i></a><a href='posts.php?source=edit_post&p_id={$post_id}'><i class='fa fa-pencil-square-o fa-lg' aria-hidden='true'></i></td>
+                                                <td>$comment_id</td>
+                                                <td>$comment_author</td>
+                                                <td>$comment_content</td>
+                                                <td>$comment_email</td>
+                                                <td>$comment_status</td>
+                                                <td>$comment_post_id</td>
+                                                <td>$comment_date</td>
+                                                <td>Approve</td>
+                                                <td>Unapprove</td>
+                                                <td><a href='posts.php?delete={$comment_id}'><i class='fa fa-trash-o fa-2x' aria-hidden='true'></i></a><a href='posts.php?source=edit_post&p_id={$comment_id}'><i class='fa fa-pencil-square-o fa-lg' aria-hidden='true'></i></td>
                                             </tr>";
                                     }        
                                             
@@ -63,12 +63,12 @@
 
 <?php
 
-    if(isset($_GET['delete'])){
-        $the_post_id = $_GET['delete'];
+    // if(isset($_GET['delete'])){
+    //     $the_post_id = $_GET['delete'];
 
-    $query = "DELETE FROM posts WHERE post_id = {$the_post_id}";
-    $delete_query = mysqli_query($connection, $query);
-    }
+    // $query = "DELETE FROM posts WHERE post_id = {$the_post_id}";
+    // $delete_query = mysqli_query($connection, $query);
+    // }
 
 
 ?>
