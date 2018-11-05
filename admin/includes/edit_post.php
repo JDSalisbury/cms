1cbb252379286d2c7905146135ccac9d9a5049f6
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-11-05 13:56:44 -0500

    can now update posts, with images and all

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -5,7 +5,7 @@
        $post_to_edit_id = $_GET['p_id'];
     }
 
-    $query = "SELECT * FROM posts ";
+    $query = "SELECT * FROM posts WHERE post_id = $post_to_edit_id";
     $select_post_by_id = mysqli_query($connection, $query);                                                            
     while($row = mysqli_fetch_assoc($select_post_by_id)){
         $post_id = $row["post_id"];
@@ -19,6 +19,46 @@
         $post_category_id = $row["post_category_id"];
         $post_content = $row["post_content"];
     }
+
+    if(isset($_POST['update_post'])) {
+
+        $post_author = $_POST['post_author'];
+        $post_title = $_POST['post_title'];
+        $post_category_id = $_POST['post_category_id'];
+        $post_status = $_POST['post_status'];
+        $post_image = $_FILES['image']['name'];
+        $post_image_temp = $_FILES['image']['tmp_name'];
+        $post_content = $_POST['post_content'];
+        $post_tags = $_POST['post_tags'];
+
+        move_uploaded_file($post_image_temp, "../images/$post_image");
+
+        if(empty($post_image)) {
+            $image_query = "SELECT * from posts WHERE post_id = $post_to_edit_id";
+            $select_image = mysqli_query($connection, $query);
+            while($row = mysqli_fetch_array($select_image)) {
+                $post_image = $row['post_image'];
+            }
+        }
+
+        $query = "UPDATE posts SET ";
+        $query .="post_title = '{$post_title}', ";
+        $query .="post_category_id = '{$post_category_id}', ";
+        $query .="post_date = now(), ";
+        $query .="post_author = '{$post_author}', ";
+        $query .="post_status = '{$post_status}', ";
+        $query .="post_tags = '{$post_tags}', ";
+        $query .="post_content = '{$post_content}', ";
+        $query .="post_image = '{$post_image}' ";
+        $query .="WHERE post_id = {$post_to_edit_id} ";
+
+        $update_post_query = mysqli_query($connection, $query);
+
+        if(!$update_post_query) {
+            die("Query Failed " . mysqli_error($connection));
+        }
+
+    }
 ?>
 
 
@@ -26,33 +66,29 @@
 <form action="" method="POST" enctype="multipart/form-data">
 
     <div class="form-group">
-        <label for="title">Post Title</label>
-        <input value="<?php echo $post_title; ?>" type="text" class="form-control" name="title">
+        <label for="post_title">Post Title</label>
+        <input value="<?php echo $post_title; ?>" type="text" class="form-control" name="post_title">
     </div>
 
     <div class="form-group">
         
-        <select name="post_category" id="post_category">
+        <select name="post_category_id" id="post_category">
             <?php
-
                 $query = "SELECT * FROM categories";
-                $select_categories = mysqli_query($connection, $query);
-                            
+                $select_categories = mysqli_query($connection, $query);           
                 while($row = mysqli_fetch_assoc($select_categories)){
                     $cat_title = $row["cat_title"];
                     $cat_ID = $row["cat_id"];
-
                     echo "<option value='{$cat_ID}'>{$cat_title}</option>";
                 }
-
             ?>
         </select>
         
     </div>
 
     <div class="form-group">
-        <label for="author">Post Author</label>
-        <input value="<?php echo $post_author; ?>" type="text" class="form-control" name="author">
+        <label for="post_author">Post Author</label>
+        <input value="<?php echo $post_author; ?>" type="text" class="form-control" name="post_author">
     </div>
 
     <div class="form-group">
@@ -77,7 +113,7 @@
     </div>
 
     <div class="form-group">
-        <input class="btn btn-primary" type="submit" name="create_post" value="PUBLISH">
+        <input class="btn btn-primary" type="submit" name="update_post" value="UPDATE">
     </div>
 
 </form>
