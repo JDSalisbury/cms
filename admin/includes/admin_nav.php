27ac2d0b0d86a7a90ac4b3176868eb6e85beaa63
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-11-06 13:41:30 -0500

    just creating the names and frameworks for the user pages

--- admin/includes/admin_nav.php
+++ admin/includes/admin_nav.php
@@ -59,14 +59,14 @@
                         <a href="javascript:;" data-toggle="collapse" data-target="#demo1"><i class="fa fa-fw fa-arrows-v"></i> USERS <i class="fa fa-fw fa-caret-down"></i></a>
                         <ul id="demo1" class="collapse">
                             <li>
-                                <a href="#">Dropdown Item</a>
+                                <a href="users.php?source=add_user">ADD</a>
                             </li>
                             <li>
-                                <a href="#">Dropdown Item</a>
+                                <a href="users.php">VIEW</a>
                             </li>
                         </ul>
                     </li>
-                    <li class="active">
+                    <li>
                         <a href="comments.php"><i class="fa fa-fw fa-comments-o"></i>COMMENTS</a>
                     </li>
                     
