9dfc8d8b522fe9d05a90a54b91124c51d4fb8f50
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-11-06 16:54:19 -0500

    created the form on the index page to login

--- includes/widget-well.php
+++ includes/widget-well.php
@@ -1,4 +1,13 @@
 <div class="well">
-    <h4>Side Widget Well</h4>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
+    <h4>Login</h4>
+    <form action="includes/login.php" method="POST">
+        <div class="form-group">
+            <input name="username" type="text" class="form-control" placeholder="Enter Username">
+        </div>
+
+        <div class="input-group">
+            <input name="password" type="password" class="form-control" placeholder="Enter Password">
+            <span class="input-group-btn"><button class="btn btn-primary" name="login" type="submit">ENTER</button></span>
+        </div>
+    </form>
 </div>
