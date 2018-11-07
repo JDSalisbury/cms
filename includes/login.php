b883f9b8e171e4a86295dbac7b480e70c98ee2c2
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-11-07 10:48:24 -0500

    php does truthy so yeah

--- includes/login.php
+++ includes/login.php
@@ -30,7 +30,7 @@ if(isset($_POST['login'])){
         
     }
     
-    if($username == $db_username && $password == $db_password){
+    if($username === $db_username && $password === $db_password){
         $_SESSION['username'] = $db_username;
         $_SESSION['firstname'] = $db_user_firstname;
         $_SESSION['lastname'] = $db_user_lastname;
