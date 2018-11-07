6f64ea49cb5180cc950cedb0e910c7023d63f559
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-11-07 09:52:53 -0500

    setting up a session for a logged in user

--- includes/login.php
+++ includes/login.php
@@ -1,4 +1,6 @@
-<?php include "db.php"; ?>
+<?php include "db.php"; 
+    session_start();
+?>
 
 <?php
 
@@ -27,9 +29,11 @@ if(isset($_POST['login'])){
         
     }
     
-    if($username !== $db_username || $password !== $db_password){
-       header("Location: ../index.php ");
-    } elseif($username == $db_username && $password == $db_password){
+    if($username == $db_username && $password == $db_password){
+        $_SESSION['username'] = $db_username;
+        $_SESSION['firstname'] = $db_user_firstname;
+        $_SESSION['lastname'] = $db_user_lastname;
+        $_SESSION['user_role'] = $db_user_role;
         header("Location: ../admin");
     } else{
         header("Location: ../index.php ");
