e670cdf0c997e7e60c19848be3929915b7b722d4
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-11-08 14:34:55 -0500

    kind of able to send email, but only if certain things are set up

--- includes/contact.php
+++ includes/contact.php
@@ -1,44 +1,34 @@
 <?php
+    $message = 'This will not work unless an SMTP is set up';
 
     if(isset($_POST['submit'])){
-        $username = $_POST['username'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];
 
-        if(!empty($username) && !empty($email) && !empty($password) ){
+        $to = "Where you want the email to be sent";
+        $headers = "From: " . $_POST['email'];
+        $subject = $_POST['subject'];
+        $body = $_POST['body'];
 
-            $username = mysqli_real_escape_string($connection, $username);
-            $email = mysqli_real_escape_string($connection, $email);
-            $password = mysqli_real_escape_string($connection, $password);
+        if(!empty($headers) && !empty($subject) && !empty($body) ){
+
+            $headers = mysqli_real_escape_string($connection, $headers);
+            $subject = mysqli_real_escape_string($connection, $subject);
+            $body = mysqli_real_escape_string($connection, $body);
     
-            $query = "SELECT randsalt FROM users";
-            $select_randsalt_query = mysqli_query($connection, $query);
+            
+            mail($to, $subject, $body, $headers);
     
-            if(!$select_randsalt_query){
-                die("Query Failed" . mysqli_error($connection));
-            }
-    
-            $row = mysqli_fetch_array($select_randsalt_query);
-            $salt = $row['randsalt'];
-            $password = crypt($password, $salt);
-    
-            $query = "INSERT INTO users (username, user_email, user_password, user_role) ";
-            $query .= "VALUES('{$username}','{$email}', '{$password}', 'subscriber') ";
-            $register_user_query = mysqli_query($connection, $query);
-            if(!$register_user_query) {
-                die("Query Failed" . mysqli_error($connection) . ' ' . mysqli_errno($connection));
-            }
+            
             $message = "You are Registered";
         } 
+
     } else {
 
-        $message = '';
     }
 ?>
 
 
    
-<section id="login">       
+<section id="email">       
     <div class="form-wrap">
         <h1>Contact</h1>
         <form role="form" action="" method="POST" id="about-form" autocomplete="off">
